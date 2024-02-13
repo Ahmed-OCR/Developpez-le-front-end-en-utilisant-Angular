5d917b24f39e8596938cefbb5ab8d9b89d285f0e
Ahmed BEN ROUAG <ahmed.benrouag@gmail.com>
2024-02-13 11:48:30 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { take } from 'rxjs';
 import { OlympicService } from './core/services/olympic.service';
+import { Olympic } from './core/models/Olympic';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,14 @@ import { OlympicService } from './core/services/olympic.service';
 })
 export class AppComponent implements OnInit {
   constructor(private olympicService: OlympicService) {}
+  olympics!: Olympic[];
 
   ngOnInit(): void {
-    this.olympicService.loadInitialData().pipe(take(1)).subscribe();
+    this.olympicService
+      .loadInitialData()
+      .pipe(take(1))
+      .subscribe((olympics: Olympic[]) => {
+        this.olympics = olympics;
+      });
   }
 }
