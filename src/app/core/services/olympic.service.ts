12ef5370c41c40c98ca51e7bd6a1282eff188977
Ahmed BEN ROUAG <ahmed.benrouag@gmail.com>
2024-02-22 11:36:18 +0100

    - Refactor code

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,32 +1,43 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable} from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
-import {Olympic} from "../models/Olympic";
+import { Olympic } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl: string = './assets/mock/olympic.json';
-  private olympics$: BehaviorSubject<Olympic[]> = new BehaviorSubject<Olympic[]>([]);
+  private olympics$: BehaviorSubject<Olympic[]> = new BehaviorSubject<
+    Olympic[]
+  >([]);
 
   constructor(private http: HttpClient) {}
 
-  loadInitialData():Observable<Olympic[]> {
+  loadInitialData(): Observable<Olympic[]> {
     return this.http.get<Olympic[]>(this.olympicUrl).pipe(
-      tap((value: Olympic[]) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next([]);
-        return caught;
-      })
+      tap((value: Olympic[]) => this.olympics$.next(value)), // Met à jour les données olympiques en cas de succès
+      catchError(this.handleError), // Gère les erreurs
     );
   }
 
-  getOlympics():Observable<Olympic[]> {
+  getOlympics(): Observable<Olympic[]> {
     return this.olympics$.asObservable();
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage: string = 'Une erreur est survenue.';
+    if (error.error instanceof ErrorEvent) {
+      // Erreur côté client
+      errorMessage = `Erreur : ${error.error.message}`;
+    } else {
+      // Erreur côté serveur
+      errorMessage = `Code d'erreur : ${error.status}, message : ${error.message}`;
+    }
+    console.error(errorMessage);
+    // Met à jour les données olympiques avec un tableau vide pour indiquer qu'il y a eu une erreur
+    this.olympics$.next([]);
+    return throwError(() => errorMessage); // Retourne une erreur observable
   }
 }
